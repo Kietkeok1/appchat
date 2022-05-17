50d23e3b831905fac09a473f7987bd2bd216e6a8
you name <kiethuynh2001bt@gmail.com>
2022-05-17 21:57:18 +0700

    first commit

---
+++ app/src/main/java/com/example/chattapp/activities/SignInActivity.java
@@ -0,0 +1,116 @@
+package com.example.chattapp.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chattapp.databinding.ActivitySignInBinding;
+import com.example.chattapp.utilities.Constants;
+import com.example.chattapp.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class SignInActivity extends AppCompatActivity {
+    private ActivitySignInBinding binding;
+    private PreferenceManager preferenceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        if (preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+        binding.buttonSignIn.setOnClickListener(v -> {
+            if (isValidSignInDetails()) {
+                signIn();
+            }
+        });
+
+    }
+    private void signIn() {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult() != null
+                            && task.getResult().getDocuments().size() > 0) {
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                        preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                        preferenceManager.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
+                        preferenceManager.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    } else {
+                        loading(false);
+                        showToast(" Khong the dang nhap, nhap lai pass di ban");
+                    }
+                });
+    }
+
+    private void loading(Boolean isLoading) {
+        if (isLoading) {
+            binding.buttonSignIn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonSignIn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(),message, Toast.LENGTH_SHORT).show();
+    }
+    private Boolean isValidSignInDetails() {
+        if (binding.inputEmail.getText().toString().trim().isEmpty()) {
+            showToast(" Nhập email");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()) {
+            showToast(" Nhập lại email đi pro ");
+            return false;
+        }
+        else if (binding.inputPassword.getText().toString().trim().isEmpty()) {
+            showToast(" Nhap password");
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+
+}
+//    binding.buttonSignIn.setOnClickListener(v -> addDataToFirestore());
+//    private void addDataToFirestore() {
+//        FirebaseFirestore database = FirebaseFirestore.getInstance();
+//        HashMap<String, Object> data = new HashMap<>();
+//        data.put("first_name", "kiethuynh");
+//        data.put("last_name", "minhanh");
+//        database.collection("users")
+//                .add(data)
+//                .addOnSuccessListener(documentReference -> {
+//                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
+//                })
+//                .addOnFailureListener(exception -> {
+//                    Toast.makeText(getApplicationContext(),exception.getMessage(), Toast.LENGTH_SHORT).show();
+//                });
+//    }
---
+++ app/src/main/java/com/example/chattapp/adapters/ChatAdapter.java
@@ -0,0 +1,103 @@
+package com.example.chattapp.adapters;
+
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chattapp.databinding.ItemContainerReceivedMessageBinding;
+import com.example.chattapp.databinding.ItemContainerSentMessageBinding;
+import com.example.chattapp.models.ChatMessage;
+
+import java.util.List;
+
+
+public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final Bitmap receiverProfileImage;
+    private final List<ChatMessage> chatMessages;
+    private final String senderId;
+
+    public static final int VIEW_TYPE_SENT = 1;
+    public static final int VIEW_TYPE_RECEIVED = 2;
+
+    public ChatAdapter(Bitmap receiverProfileImage, List<ChatMessage> chatMessages, String senderId) {
+        this.receiverProfileImage = receiverProfileImage;
+        this.chatMessages = chatMessages;
+        this.senderId = senderId;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       if (viewType == VIEW_TYPE_SENT) {
+           return new SenmessageViewHolder(
+                   ItemContainerSentMessageBinding.inflate(
+                           LayoutInflater.from(parent.getContext()),
+                           parent,
+                           false
+                   )
+           );
+       } else {
+           return new ReceiveMessageViewHolder(
+                   ItemContainerReceivedMessageBinding.inflate(
+                           LayoutInflater.from(parent.getContext()),
+                           parent,
+                           false
+                   )
+           );
+       }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (getItemViewType(position) == VIEW_TYPE_SENT) {
+            ((SenmessageViewHolder) holder).setData(chatMessages.get(position));
+        } else {
+
+            ((ReceiveMessageViewHolder) holder).setData(chatMessages.get(position), receiverProfileImage);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessages.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+      if (chatMessages.get(position).senderId.equals(senderId)) {
+          return VIEW_TYPE_SENT;
+      } else {
+          return VIEW_TYPE_RECEIVED;
+      }
+    }
+
+    static class SenmessageViewHolder extends RecyclerView.ViewHolder {
+        private final ItemContainerSentMessageBinding binding;
+        SenmessageViewHolder(ItemContainerSentMessageBinding itemContainerSentMessageBinding) {
+            super(itemContainerSentMessageBinding.getRoot());
+            binding = itemContainerSentMessageBinding;
+
+        }
+        void setData(ChatMessage chatMessage) {
+            binding.textMessage.setText(chatMessage.message);
+            binding.textDataTime.setText(chatMessage.dateTime);
+        }
+    }
+    static class ReceiveMessageViewHolder extends RecyclerView.ViewHolder {
+        private final ItemContainerReceivedMessageBinding binding;
+
+        ReceiveMessageViewHolder(ItemContainerReceivedMessageBinding itemContainerReceivedMessageBinding) {
+            super(itemContainerReceivedMessageBinding.getRoot());
+            binding = itemContainerReceivedMessageBinding;
+        }
+        void setData(ChatMessage chatMessage, Bitmap receiverProfileImage) {
+            binding.textMessage.setText(chatMessage.message);
+            binding.textDateTime.setText(chatMessage.dateTime);
+            binding.imageProfile.setImageBitmap(receiverProfileImage);
+        }
+    }
+}
